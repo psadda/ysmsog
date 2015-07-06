d199a72ab3355787dba206c5366c75fb806751f3
Praneeth Sadda <psadda@gmail.com>
2015-07-06 10:20:02 -0400

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,20 @@
+# If you do not have OpenSSL installed, update
+# the following line to use "http://" instead
+source 'https://rubygems.org'
+
+gem 'middleman', '~> 3.3.12'
+
+gem 'middleman-vcs-time'
+gem 'middleman-spellcheck'
+gem 'middleman-sitemap'
+gem 'middleman-ogp'
+
+gem 'htmlentities'
+gem 'redcarpet'
+
+gem 'bitters'
+gem 'bourbon'
+gem 'neat'
+
+# Windows does not come with time zone data
+gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby]
---
+++ config.rb
@@ -0,0 +1,61 @@
+###
+# Compass
+###
+
+# Change Compass configuration
+# compass_config do |config|
+#   config.output_style = :compact
+# end
+
+###
+# Page options, layouts, aliases and proxies
+###
+
+# Per-page layout changes:
+#
+# With no layout
+# page "/path/to/file.html", :layout => false
+#
+# With alternative layout
+# page "/path/to/file.html", :layout => :otherlayout
+#
+# A path which all have the same layout
+# with_layout :admin do
+#   page "/admin/*"
+# end
+
+# Proxy pages (https://middlemanapp.com/advanced/dynamic_pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
+#  :which_fake_page => "Rendering a fake page with a local variable" }
+
+activate :automatic_image_sizes
+activate :directory_indexes
+
+set :css_dir, 'stylesheets'
+set :js_dir, 'javascripts'
+set :images_dir, 'images'
+
+set :markdown_engine, :redcarpet
+
+###
+# Helpers
+###
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+
+# Build-specific configuration
+configure :development do
+
+end
+
+configure :build do
+  activate :minify_css
+  activate :minify_javascript
+  activate :asset_hash
+  activate :spellcheck
+end
