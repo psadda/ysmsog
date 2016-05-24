3f3986c82edd7c2fc03cc06879de159fff3e96b3
Praneeth Sadda <psadda@gmail.com>
2016-05-23 21:49:13 -0400

    Upgrade to Middleman 4

--- Gemfile
+++ Gemfile
@@ -2,12 +2,7 @@
 # the following line to use "http://" instead
 source 'https://rubygems.org'
 
-gem 'middleman', '~> 3.3.12'
-
-gem 'middleman-vcs-time'
-gem 'middleman-spellcheck'
-gem 'middleman-sitemap'
-gem 'middleman-ogp'
+gem 'middleman', '~> 4.1.8'
 
 gem 'htmlentities'
 gem 'redcarpet'
--- config.rb
+++ config.rb
@@ -1,33 +1,3 @@
-###
-# Compass
-###
-
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# Proxy pages (https://middlemanapp.com/advanced/dynamic_pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
-
 activate :automatic_image_sizes
 activate :directory_indexes
 activate :relative_assets
@@ -41,23 +11,7 @@ set :relative_links, true
 
 set :markdown_engine, :redcarpet
 
-###
-# Helpers
-###
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
-
-# Build-specific configuration
-configure :development do
-
-end
-
-configure :build do
+configure :production do
   activate :minify_css
   activate :minify_javascript
   activate :asset_hash
